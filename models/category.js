e50a98b3abf9bede8b296218bd17933f45199a67
Ali Hassan <ali.hassan@garonz.com>
2020-06-02 20:16:01 +0200

    Generating the models automatically using "sequelize-auto" https://www.npmjs.com/package/sequelize-auto

---
+++ models/category.js
@@ -0,0 +1,18 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('category', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    }
+  }, {
+    tableName: 'category'
+  });
+};
---
+++ models/product.js
@@ -0,0 +1,38 @@
+/* jshint indent: 2 */
+
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('product', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    category_id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
+  }, {
+    tableName: 'product'
+  });
+};
